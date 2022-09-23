aef3a5832f2ba86a864bf89c85d043de83312331
lizq <lizq@epshealth.com>
2022-09-23 18:05:29 +0800

    feat: 新增添加模板功能

--- bin/cli.js
+++ bin/cli.js
@@ -4,10 +4,14 @@ import { program } from "commander";
 import myHelp from "../lib/core/help.js";
 import myList from "../lib/core/list.js";
 import myInit from "../lib/core/init.js";
+import myAdd from "../lib/core/add.js";
 
 myHelp(program);
 myList(program);
+
 myInit(program);
+
+myAdd(program)
 
 program.parse(process.argv);
 
