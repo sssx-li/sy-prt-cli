987141393b0620e234fa9095008b7b382bdd2129
lizhiquan <842048365@qq.com>
2022-09-23 23:47:33 +0800

    perf: 代码优化

--- bin/cli.js
+++ bin/cli.js
@@ -1,17 +1,27 @@
 #! /usr/bin/env node
 
 import { program } from "commander";
+import { readFile } from "fs/promises";
+
 import myHelp from "../lib/core/help.js";
 import myList from "../lib/core/list.js";
 import myInit from "../lib/core/init.js";
 import myAdd from "../lib/core/add.js";
 
+// 获取版本号
+program.version(
+  JSON.parse(await readFile(new URL("../package.json", import.meta.url)))
+    .version,
+  "-V, --version"
+);
+
 myHelp(program);
+
 myList(program);
 
 myInit(program);
 
-myAdd(program)
+myAdd(program);
 
 program.parse(process.argv);
 
